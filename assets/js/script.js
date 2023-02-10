26f90903b3c0a57354176f0faf3f7e290d2a653a
Hailey Flora <Haileycflora@gmail.com>
2023-02-10 12:34:26 -0500

    add code to getOptions function

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,12 +9,12 @@ var number; //number
 //assigning the password criteria variables to values
 //question: is this correct to be writing these in arrays?
 //question: special characters can only be an array, correct?
-var upperAlph= ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var lowerAlph= ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperAlph = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerAlph = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 // change to strings with commas
-var specChar= [!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"]
-var number= ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specChar = "!, #, $, %, &, (, ), *, +, -, ., /, :, ;, <, =, >, ?, @, [, ], ^, _, {, |, }, ~,"
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -29,12 +29,12 @@ function generatePassword() {
     userArray.push(lowerAlph)
   } else if (options.upper) {
     userArray.push(upperAlph)
-  } else if (options.special)
-    userArray.push(specChar)
+  } else (options.special)
+  userArray.push(specChar)
 
   var passwd = ""
 
-  for(var i=0; i<options.length; i++) {
+  for (var i = 0; i < options.length; i++) {
     // this line is choosing a random array from our user array
     var randIndex = Math.floor(Math.random() * userArray.length)
     var workingArray = userArray[randIndex]
@@ -43,32 +43,48 @@ function generatePassword() {
   }
 
   return passwd
-  
+
 }
 
-function getOptions() { 
-  var length=prompt("Please provide length of your desired password. Note, length should be between 8-128 characters.", "8")
+function getOptions() {
+  var length = prompt("Please provide length of your desired password. Note, length should be between 8-128 characters.", "8")
   // if length isn't numeric alert the user that we need a number return null (typeof)
   // if length is less than 8 return null
   // if length is greater than 128 return null
   if (length < 8) {
-    alert("Password length needs to be between 8 and 128 characters.")
+    alert("Password length needs to be between 8 and 128 characters. Please try again.")
+    return null
+  }
+  if (length > 128) {
+    alert("Password length needs to be between 8 and 128 characters. Please try again.")
+    return null
+  }
+  if (length !== number) {
+    alert("Password length must be specified as a number between 8 and 128. Please try again.")
     return null
   }
 
-  var numeric=confirm("Would you like to include numbers in your password?")
-  console.log(numeric); 
-  //write 3 more confirm for other var to meet criteria
+}
+length = (prompt("Please provide length of your desired password. Note, length should be between 8-128 characters."));
 
-  // if no options are chosen (all = false) alert 'need at least one type of char' return null
-  // return an array of all the options
-  return {
-    length,
-    numeric, 
-    special, 
-    uppper, 
-    lower
-  }
+var numeric = confirm("Would you like to include numbers in your password?")
+console.log(numeric);
+var lower = confirm("Would you like to include lowecase letters in your password?")
+console.log(lower);
+var upper = confirm("Would you like to include uppcase letters in your password?")
+console.log(upper);
+var special = confirm("Would you like to include special characters in your password?")
+console.log(special);
+//write 3 more confirm for other var to meet criteria
+
+// if no options are chosen (all = false) alert 'need at least one type of char' return null
+// return an array of all the options
+return {
+  length,
+  numeric,
+  special,
+  upper,
+  lower
 }
 
 
