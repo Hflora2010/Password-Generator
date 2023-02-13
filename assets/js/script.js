479dca103eed304ab92e2e1020f6c0b98e4a04f3
Hailey Flora <Haileycflora@gmail.com>
2023-02-13 16:22:51 -0500

    modify commenting. Clarify code for users

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
-// Assignment code here
 
 //Declaring password criteria variables 
 var upperAlph; //uppercase letters
@@ -7,15 +6,12 @@ var specChar; //special characters
 var number; //number
 
 //assigning the password criteria variables to values
-//question: is this correct to be writing these in arrays?
-//question: special characters can only be an array, correct?
 var upperAlph = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowerAlph = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
-// change to strings with commas
 var specChar = "!,#,$,%,&,(,),*,+,-,.,/,:,;,<,=,>,?,@,[,],^,_,{,|,},~,".split(",")
 var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
+//accessing random number in array by generating random float from zero to array's length and rounding to whole number. Synopsis of this function found/understood at: https://stackoverflow.com/questions/43267033/understanding-the-use-of-math-floor-when-randomly-accessing-an-array 
 function random(arr) {
   return arr[Math.floor(Math.random()* arr.length) ]
 }
@@ -31,7 +27,6 @@ function generatePassword() {
   var passwd = ""
 
 
-  // continue do build this out
   if (options.numeric) {
     userArray.push(...number)
     passwd += random(number)
@@ -57,12 +52,9 @@ function generatePassword() {
   return passwd
 
 }
-
+//converting arugment into a string, parsing the string and returning a number or NaN, in some cases where while loop will come back into play.
 function getOptions() {
   var length = parseInt(prompt("Please provide length of your desired password. Note, length should be between 8-128 characters.", "8"))
-  // if length isn't numeric alert the user that we need a number return null (typeof)
-  // if length is less than 8 return null
-  // if length is greater than 128 return null
   while (length < 8 || length > 128 || isNaN(length)) {
    length = parseInt(prompt("Please provide length of your desired password. Note, length should be between 8-128 characters.", "8"))
   }
@@ -75,9 +67,7 @@ function getOptions() {
   console.log(upper);
   var special = confirm("Would you like to include special characters in your password?")
   console.log(special);
-  //write 3 more confirm for other var to meet criteria
 
-  // if no options are chosen (all = false) alert 'need at least one type of char' return null
   // return an array of all the options
   return {
     length,
@@ -97,54 +87,3 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
-
-// Add event listener to generate button
-
-
-
-// function generatePassword() {
-
-//   // prompt user for password length
-//   var length = prompt("How many characters would you like your password to be?")
-//   // check if length is between 8 and 128
-//   if (length < 8 || length > 128) {
-//     alert("Password must be between 8 and 128 characters")
-//     return
-//   }
-
-//   // prompt user for character types
-//   var options = {
-//     numeric: confirm("Would you like to include numbers?"),
-//     lower: confirm("Would you like to include lowercase letters?"),
-//     upper: confirm("Would you like to include uppercase letters?"),
-//     special: confirm("Would you like to include special characters?")
-//   }
-
-//   var userArray = []
-
-//   // continue do build this out
-//   if (options.numeric) {
-//     userArray.push(number)
-//   } else if (options.lower) {
-//     userArray.push(lowerAlph)
-//   } else if (options.upper) {
-//     userArray.push(upperAlph)
-//   } else (options.special)
-//   userArray.push(specChar)
-
-//   var passwd = ""
-
-  
-
-  
-
-// }
-
-// for (var i = 0; i < length; i++) {
-//   var randomIndex = Math.floor(Math.random() * userArray.length)
-//   var randomChar = Math.floor(Math.random() * userArray[randomIndex].length)
-//   passwd += userArray[randomIndex][randomChar]
-// }
-
-// return passwd
